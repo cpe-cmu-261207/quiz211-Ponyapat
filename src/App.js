f60825979ac17620936a48a5e7984889820b932e
Ponyapat <42263205+Ponyapat@users.noreply.github.com>
2021-02-02 13:59:52 +0700

    test1

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { useEffect } from "react";
+
 function App() {
   const persons = [
     {
@@ -9,6 +11,14 @@ function App() {
       name: "Alice",
       gender: "male",
       age: "20"
+    }
+  ];
+
+  const user = [
+    {
+      name: "Poonyapat Anusonthi",
+      gender: "male",
+      age: "22"
     }
   ];
   return (
@@ -31,9 +41,40 @@ function App() {
         </tbody>
       </table>
 
+      <table class="table is-bordered mb-3">
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <th>Gender</th>
+            <th>Age</th>
+          </tr>
+          <tr>
+            <td>Alice</td>
+            <td>male</td>
+            <td>20</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <table class="table is-bordered mb-3">
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <th>Gender</th>
+            <th>Age</th>
+          </tr>
+          <tr>
+            <td>Poonyapat Anusonthi</td>
+            <td>male</td>
+            <td>22</td>
+          </tr>
+        </tbody>
+      </table>
       {/* Code me please! */}
       <h3 class="title is-3">ID Counter</h3>
-      <p>YOUR ID HERE</p>
+
+      <p>620612156</p>
+
       <button>-</button>
       <button>reset</button>
       <button>+</button>
